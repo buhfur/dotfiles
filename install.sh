c9576875096474cc73915720563d0b6c994e70e7
buhfur <ryanmcvicker@protonmail.com>
2024-11-24 17:42:22 -0500

    changes

--- install.sh
+++ install.sh
@@ -4,60 +4,54 @@
 
 DOTFILE_DIR="${1:-.}" # defaults to current working directory 
 EXCLUDED_FILES=(".gitignore" ".install.sh.swp") # Files which shoulden't be copied / made symbolic links to 
-#DOTFILES=(".bashrc" ".vimrc", ".zshrc" ".zshenv" ".bash_profile")
 
 for DOTFILE in "$DOTFILE_DIR"/.*; do
-
-    # TODO: Absolute and Relative paths for the dotfile contained within repo. Absolute path is required for the symbolic link to be created 
-
-    # Verify 
-    # TODO: create list which specifies which files should be ignored
-
-
 
     DOTFILE_REL_PATH="${DOTFILE#./}"
     DOTFILE_ABS_PATH="$(realpath "$DOTFILE")" 
     DOTFILE_BACKUP_DIR=/tmp/dotfile-backup # default dir for backups 
-     
-    # copy oh-my-zsh config manually 
-    if [ $DOTFILE_REL_PATH == ".oh-my-zsh" ]; then
-        # Check for existing oh-my-zsh config 
-        if [ -d $HOME/.oh-my-zsh ]; then
-            echo "cp -vr $HOME/.oh-my-zsh $DOTFILE_BACKUP_DIR"
-            # Create symlink to oh-my-zsh config in repo  
-            echo "sudo ln -sf $DOTFILE_ABS_PATH $HOME/.oh-my-zsh"
+
+    if [ $DOTFILE_REL_PATH == ".oh-my-zsh" ]; then # Copy oh-my-zsh config manually 
+        if [ -d $HOME/.oh-my-zsh ]; then # Check for existing oh-my-zsh config 
+            cp -vr $HOME/.oh-my-zsh $DOTFILE_BACKUP_DIR
+            echo "INFO: all ohmyzsh files to $DOTFILE_BACKUP_DIR successfully"
+            rm -r $HOME/.oh-my-zsh # Removes existing directory 
+            echo "INFO: Creating symbolic link to ohmyzsh config..."
+            sudo ln -svf -t $HOME $DOTFILE_ABS_PATH # Create symlink to oh-my-zsh config in repo  
+            echo "INFO: ohmyzsh links created successfully "
+            
         fi
     fi
 
-    # Checks if the dotfile exists
-    if [[ -f $DOTFILE_ABS_PATH ]]; then
 
-        # Check if dotfile exists in users $HOME if it does , copy to /tmp 
-        if [[ -f $HOME/$DOTFILE_REL_PATH || -d $HOME/$DOTFILE_REL_PATH ]]; then
+    if [[ -f $DOTFILE_ABS_PATH ]]; then # Checks if the dotfile exists
+
+        if [[ -f $HOME/$DOTFILE_REL_PATH || -d $HOME/$DOTFILE_REL_PATH ]]; then # Check if dotfile exists in users $HOME if it does , copy to /tmp 
             if [ ! -d $DOTFILE_BACKUP_DIR ]; then # Creates backup directory which holds a copy of current dotfiles
-                echo "No $DOTFILE_BACKUP_DIR found , Creating... "
+                echo "INFO: Target directory : $DOTFILE_BACKUP_DIR not found , Creating... "
                 mkdir $DOTFILE_BACKUP_DIR
-                echo "Successfully created $DOTFILE_BACKUP_DIR"
+                echo "INFO: Successfully created $DOTFILE_BACKUP_DIR"
             fi
-
         else
             echo "INFO: $HOME/$DOTFILE_REL_PATH was not found. Skipping..."
         fi
 
-        # Copy old dotfiles to backup-dir 
-        if [ -f $HOME/$DOTFILE_REL_PATH ]; then
 
-            echo "cp -rv $HOME/$DOTFILE_REL_PATH $DOTFILE_BACKUP_DIR"
-            echo "Target : $HOME/$DOTFILE_REL_PATH copied to $DOTFILE_BACKUP_DIR"
+        if [ -f $HOME/$DOTFILE_REL_PATH ]; then # Copy old dotfiles to backup-dir 
+            cp -rv $HOME/$DOTFILE_REL_PATH $DOTFILE_BACKUP_DIR
+            echo "INFO: Target $HOME/$DOTFILE_REL_PATH copied to $DOTFILE_BACKUP_DIR successfully"
         fi
 
-        # Check if the dotfile is in the EXCLUDED_FILES list 
 
-        if [[ "${EXCLUDED_FILES[@]}" =~ "${DOTFILE_REL_PATH}" ]]; then 
-            echo "INFO: Ignored Target $DOTFILE_REL_PATH detected, skipping..."
+        if [[ "${EXCLUDED_FILES[@]}" =~ "${DOTFILE_REL_PATH}" ]]; then # Check if the dotfile is in the EXCLUDED_FILES list 
+            echo "INFO: Target $DOTFILE_REL_PATH detected in ignore list, skipping..."
         else
-            echo "sudo ln -sf $DOTFILE_ABS_PATH $HOME/$DOTFILE_REL_PATH"
+            echo "INFO: Creating symbolic links to dotfiles..."
+            sudo ln -svf $DOTFILE_ABS_PATH $HOME/$DOTFILE_REL_PATH
+            echo "INFO: Successfully created symbolic links to dotfiles! "
         fi
+
+
     fi
 
 done
