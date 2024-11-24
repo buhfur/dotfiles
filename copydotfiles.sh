9f5e39193d9f867db81ac1c156e0995615a36fa8
buhfur <ryanmcvicker@protonmail.com>
2024-11-23 19:59:21 -0500

    changes

--- copydotfiles.sh
+++ copydotfiles.sh
@@ -2,9 +2,9 @@
 
 # Copies dotfiles from my home directory to the repo 
 
-DOTFILES=(".oh-my-zsh",".config",".vimrc",".zshrc",".zshenv",".tmux.conf")
+DOTFILES=( ".oh-my-zsh" ".config" ".vimrc" ".zshrc" ".zshenv" ".tmux.conf" )
 
 
 for FILE in ${DOTFILES[@]}; do
-    echo -n "$FILE\n"
+    cp -rv $HOME/$FILE $HOME/dotfiles
 done
